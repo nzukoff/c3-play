ae9ca5cc5ead153cc34897dd9c9e748629adf0bb
Nathan Zukoff <NathanZukoff@Nathans-MacBook-Pro.local>
2017-11-12 20:36:40 -0800

    updates everything with BB stuff

--- js/script.js
+++ js/script.js
@@ -1,78 +1,236 @@
-var chart = c3.generate({
-    bindto: '#chart',
+var chart1 = c3.generate({
+    bindto: '#chart1',
     data: {
         x : 'x',        
         type: 'bar', 
         colors: {
-            Rural: '#0086bf',
-            Town: '#5dbc5b',
-            Suburban: '#ffd34e',
-            City: '#e75135'
+            'Top 5%': '#cdc348',
+            'Bottom 5%': '#15608d'
         },
-        names: {
-            data1: 'Name 1',
-            data2: 'Name 2'
-        },
-        url: './data/c3_test.csv'
-   },
-   axis: {
-        rotated: true,
+        columns: []
+    },
+    legend: {
+        position: 'right'
+    },
+    axis: {
         x: {
             type: 'category'
         },
         y: {
-            max: 100,
+            min: 800,
+            max: 1200,
             padding: {top:0, bottom:0}
         }
     },
-    transition: {duration: 3000}
+    transition: {duration: 2000}
 });
 
 setTimeout(function () {
-    chart.load({
+    chart1.load({
         columns: [
-            ['x', 'Top 5%', 'Bottom 5%', 'All Schools'],
-            ['Rural', 42, 24, 32],
-            ['Town', 12, 15, 14], 
-            ['Suburban', 29, 37, 33], 
-            ['City', 17, 24, 20]
+            ['x', 'Overall CASE Score', 'Classroom: 4Cs'],
+            ['Top 5%', 1118, 1066],
+            ['Bottom 5%', 989, 866], 
         ], 
     });
-}, 1000);
+}, 500);
 
-
-
-var pieChart = c3.generate({
-    bindto: '#pieChart',
+var chart2 = c3.generate({
+    bindto: '#chart2',
     data: {
         x : 'x',        
-        columns: [
-            ['x', 'Top 5%', 'Bottom 5%', 'All Schools'],
-            ['Rural', 42, 24, 32],
-            ['Town', 12, 15, 14], 
-            ['Suburban', 29, 37, 33], 
-            ['City', 17, 24, 20]
-        ], 
-        type: 'donut', 
+        type: 'bar', 
         colors: {
-            Rural: '#0086bf',
-            Town: '#5dbc5b',
-            Suburban: '#ffd34e',
-            City: '#e75135'
+            'Top 5%': '#cdc348',
+            'Bottom 5%': '#15608d'
         },
-        names: {
-            data1: 'Name 1',
-            data2: 'Name 2'
-        }
-   },
-   axis: {
-        rotated: true,
+        columns: []
+    },
+    legend: {
+        position: 'right'
+    },
+    axis: {
         x: {
             type: 'category'
         },
         y: {
-            max: 100,
-            padding: {top:0, bottom:0}
+            max:60,
+            padding: {top:0, bottom:0},
+            tick: {
+                values: [10,20,30,40,50,60],
+                format: function (d) { return `${d}%`; }
+            }
         }
-    }
+    },
+    transition: {duration: 2000}
 });
+
+setTimeout(function () {
+    chart2.load({
+        columns: [
+            ['x', 'Overall CASE Score', 'Classroom: 4Cs'],
+            ['Top 5%', 39.81, 40.31],
+            ['Bottom 5%', 39.69, 34.47], 
+        ], 
+    });
+}, 500);
+
+var chart3 = c3.generate({
+    bindto: '#chart3',
+    data: {
+        x : 'x',        
+        type: 'bar', 
+        colors: {
+            'Top 5%': '#cdc348',
+            'Bottom 5%': '#15608d'
+        },
+        columns: []
+    },
+    legend: {
+        position: 'right'
+    },
+    axis: {
+        x: {
+            type: 'category'
+        },
+        y: {
+            max:60,
+            padding: {top:0, bottom:0},
+            tick: {
+                values: [10,20,30,40,50,60],
+                format: function (d) { return `${d}%`; }
+            }
+        }
+    },
+    transition: {duration: 2000}
+});
+
+setTimeout(function () {
+    chart3.load({
+        columns: [
+            ['x', 'Overall CASE Score', 'Classroom: 4Cs'],
+            ['Top 5%', 40.66, 39.17],
+            ['Bottom 5%', 54.43, 50.78], 
+        ], 
+    });
+}, 500);
+
+var chart4 = c3.generate({
+    bindto: '#chart4',
+    data: {
+        x : 'x',        
+        type: 'bar', 
+        colors: {
+            'Top 5%': '#cdc348',
+            'Bottom 5%': '#15608d'
+        },
+        columns: []
+    },
+    legend: {
+        position: 'right'
+    },
+    axis: {
+        x: {
+            type: 'category'
+        },
+        y: {
+            max:60,
+            padding: {top:0, bottom:0},
+            tick: {
+                values: [10,20,30,40,50,60],
+                format: function (d) { return `${d}%`; }
+            }
+        }
+    },
+    transition: {duration: 2000}
+});
+
+setTimeout(function () {
+    chart4.load({
+        columns: [
+            ['x', 'Access at School', 'Access at Home'],
+            ['Top 5%', 38.62, 28.05],
+            ['Bottom 5%', 58.43, 77.79], 
+        ], 
+    });
+}, 500);
+
+var chart5 = c3.generate({
+    bindto: '#chart5',
+    data: {
+        x : 'x',        
+        type: 'bar', 
+        colors: {
+            'Top 5%': '#cdc348',
+            'Bottom 5%': '#15608d'
+        },
+        columns: []
+    },
+    legend: {
+        position: 'right'
+    },
+    axis: {
+        x: {
+            type: 'category'
+        },
+        y: {
+            max:60,
+            padding: {top:0, bottom:0},
+            tick: {
+                values: [10,20,30,40,50,60],
+                format: function (d) { return `${d}%`; }
+            }
+        }
+    },
+    transition: {duration: 2000}
+});
+
+setTimeout(function () {
+    chart5.load({
+        columns: [
+            ['x', 'FRL Percent Rate'],
+            ['Top 5%', 49.97],
+            ['Bottom 5%', 52.32], 
+        ], 
+    });
+}, 500);
+
+var chart6 = c3.generate({
+    bindto: '#chart6',
+    data: {
+        x : 'x',        
+        type: 'bar', 
+        colors: {
+            'Top 5%': '#cdc348',
+            'Bottom 5%': '#15608d'
+        },
+        columns: []
+    },
+    legend: {
+        position: 'right'
+    },
+    axis: {
+        x: {
+            type: 'category'
+        },
+        y: {
+            max:60,
+            padding: {top:0, bottom:0},
+            tick: {
+                values: [10,20,30,40,50,60],
+                format: function (d) { return `${d}%`; }
+            }
+        }
+    },
+    transition: {duration: 2000}
+});
+
+setTimeout(function () {
+    chart6.load({
+        columns: [
+            ['x', 'Environment: The 3Ps', 'Environment: Beliefs'],
+            ['Top 5%', 43.31, 44.43],
+            ['Bottom 5%', 55.02, 55.50], 
+        ], 
+    });
+}, 500);
